260a9cc3ae3b8d8b0f3de6ca76cc873642d2344b
Mario Lopez <mario.aleman@hotmail.com>
2012-09-28 15:10:16 -0500

    Se agrego AJAX con GET a AceptaUsuario

--- OEV/application/controllers/aceptaUsuarios.php
+++ OEV/application/controllers/aceptaUsuarios.php
@@ -6,12 +6,28 @@ class AceptaUsuarios extends CI_Controller {
 		$this->load->helper('url');
 		$this->load->helper('formatter');
 		$this->load->model('usuariomodel');
+        $this->load->helper('form');
 		$query['data']=$this->usuariomodel->getUsuariosPendientes();
 		$this->load->view('usuarios/header');
 		$this->load->view('usuarios/administrador/menu_administrador');
 		$this->load->view('usuarios/administrador/acepta_usuario',$query);
 		$this->load->view('usuarios/footer');
 		
+	}
+
+	public function insertData()
+	{
+
+		$post_data = $_GET;
+		print_r($post_data);die();
+ 
+ 	
+    
+		/*$posts = array();
+        foreach($_POST as $key => $value) {
+			$posts[$key] = $this->post($key);
+			echo "<p>Key: ".$key. " Value:" . $value . "</p>\n";
+        }*/
 	}
 }
 ?>
--- OEV/js/admin-users-util.js
+++ OEV/js/admin-users-util.js
@@ -5,11 +5,51 @@
 */
 $(document).ready(function(){
 	$(".accept-user").click(function() {
-		console.log($(this).parent().parent())
-
+		user_selecter_ajax_helper(true,$(this).parent().parent());
 	});                
                 
 	$(".reject-user").click(function() {
-		console.log($(this).parent().parent())
+		user_selecter_ajax_helper(false,$(this).parent().parent());
 	});   
+
+
+	function user_selecter_ajax_helper(accept,parent){
+
+
+			
+			var data={ 
+			'csrf_test_name':$('#csrf_token').attr('value'),
+			'insert': accept, 
+			'username':$(parent).find('#username').text(),
+			'idUsuario': $(parent).find('#username').attr('value'),
+			'departamento':$(parent).find('#departamento').attr('value'),
+			'tipo_usuario':$(parent).find('#tipo_usuario').val(),
+			'vista_administrador':get_real($.trim($(parent).find('#vista_administrador').text())),
+			'vista_usuario_extension':get_real($.trim($(parent).find('##vista_usuario_extension').text())),
+			'vista_supervisor_extension':get_real($.trim($(parent).find('#vista_supervisor_extension').text())),
+			'vista_profesor':get_real($.trim($(parent).find('#vista_profesor').text())),
+			'vista_cliente':get_real($.trim($(parent).find('#vista_cliente').text())),
+			'vista_legal':get_real($.trim($(parent).find('#vista_legal').text()))
+			};
+
+			$.ajax({
+			     type: "GET",
+				 datatype: 'json',
+				 contentType: "application/json; charset=utf-8",
+			     url: "aceptaUsuarios/insertData",
+				 cache:false,
+			     data: data , //assign the var here 
+			     success: function(msg){
+				        alert( "Data Saved: " + msg );
+			     }
+			});
+	}
+
+	function get_real( value ){
+		if(value == "No"){
+			return 0;
+		}else{
+			return 1;
+		}
+	}
 });
