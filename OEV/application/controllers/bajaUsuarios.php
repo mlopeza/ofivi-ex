3324b5711993a73dca78bb3a3630fad70f8590ec
Enrique <quicoide@gmail.com>
2012-11-09 01:39:20 -0600

    BAja y cambia especialidad

---
+++ OEV/application/controllers/bajaUsuarios.php
@@ -0,0 +1,36 @@
+<?php
+class BajaUsuarios extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->helper('url');
+		$this->load->helper('administrador');
+		$this->load->model('usuariomodel');
+        $this->load->helper('form');
+		$this->load->helper('security');		
+		$this->load->library('session');
+		//Toma las variables de la session
+		$vista= array('vista'=>$this->session->userdata('vista'));
+		$query['data']=$this->usuariomodel->getUsuariosActivos();
+		//Manda los datos al header
+		$this->load->view('usuarios/header',$vista);
+		$this->load->view('usuarios/administrador/menu_administrador');
+		$this->load->view('usuarios/administrador/baja_usuario',$query);
+		$this->load->view('usuarios/footer');
+		
+	}
+	public function dardeBaja(){
+		$data = $this->input->post();
+		if ($data['idUsuario'] == ''){
+			$mensaje = array('response'=>'false','mensaje' => array('mensaje'=>'Error en la actualización.'), 'errores' => validation_errors());
+			echo json_encode($mensaje);
+		}else{
+			$mensaje = array('response'=>'true','mensaje'=>'La actualizacion se realizó correctamente.');
+			//Obtiene y limpia los datos
+			$this->load->model('usuariomodel');
+			$usuario=$this->usuariomodel->darDeBaja($data['idUsuario']);
+			echo json_encode($mensaje);
+		}
+	}
+}
+?>
---
+++ OEV/application/views/usuarios/administrador/baja_usuario.php
@@ -0,0 +1,73 @@
+
+<div id="content" class="span9 section-body">
+
+                    <div id="section-body" class="tabbable"> <!-- Only required for left/right tabs -->
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab1" data-toggle="tab">Usuarios</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab1">
+                                <div class="row-fluid">
+                                    <!--Tabs2-->
+                                    <div class="span15" style="min-width:2500px;">
+                                        <div id="accordion1" class="accordion">
+                                            <div class="accordion-group">
+                                                <div class="accordion-heading">
+                                                    <a class="accordion-toggle" data-toggle="collapse" href="#notification" data-original-title="">
+                                                        <i class="icon-th icon-white"></i> <span class="divider-vertical"></span>  Solicitudes de Acceso <i class="icon-chevron-down icon-white pull-right"></i>
+                                                    </a>
+                                                </div>
+                                                <div id="notification" class="accordion-body collapse in">
+                                                    <div class="accordion-inner paddind">
+                                                        <table  class="table table-bordered table-striped pull-left" id="example" >
+                                                            <thead>
+                                                                <tr>
+                                                                    <th>Username</th>
+                                                                    <th>Campus</th>
+                                                                    <th>Escuela</th>
+                                                                    <th>Departamento</th>
+                                                                    <th>Nombre</th>
+                                                                    <th>Correo</th>
+                                                                    <th>Acción</th>
+                                                                </tr>
+                                                            </thead>
+															<?php foreach($data->result() as $row){ ?>
+
+                                                                <tr>
+                                                                    <td id="username"  value="<?php echo $row->idUsuario; ?>"><?php echo $row->Usuario; ?></td>                                                                    <td id="campus" value=""><?php echo $row->Campus; ?></td>                                                                    <td id="escuela" value=""><?php echo $row->Escuela; ?></td>
+                                                                    <td id="departamento" value="<?php echo $row->idDepartamento; ?>"><?php echo $row->Departamento; ?></td>
+                                                                    <td id="nombre"><?php echo $row->Nombre; ?></td>
+                                                                    <td id="email"><?php echo $row->Email;?></td>
+																</td>
+                                                                      <td style="text-align:center;">
+                                                                            <button class="btn btn-danger rechazar-usuario">Dar de Baja</button> 
+                                                                      </td>
+                                                                </tr>
+															<?php 
+															}; ?>
+                                                            </tbody>
+                                                            <tfoot>
+                                                                <tr>
+                                                                    <th>Username</th>
+                                                                    <th>Campus</th>
+                                                                    <th>Escuela</th>
+                                                                    <th>Departamento</th>
+                                                                    <th>Nombre</th>
+                                                                    <th>Correo</th>
+                                                                    <th>Acción</th>
+                                                                </tr>
+                                                            </tfoot>
+                                                        </table>
+                                                        <br />
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div></div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
