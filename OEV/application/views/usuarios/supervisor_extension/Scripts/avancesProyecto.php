95538228f94a34252df4099a84b1202a2973b435
Mario Lopez <mario.aleman@hotmail.com>
2012-11-16 01:53:54 -0600

    Se agregaron archivos

---
+++ OEV/application/views/usuarios/supervisor_extension/Scripts/avancesProyecto.php
@@ -0,0 +1,328 @@
+<script  type="text/javascript" >
+	$(document).ready(function(){
+		
+		//Llena el primer campo
+		$("#sgrupo").change(function() {
+						getEmpresas($(this).children('option').filter(':selected').attr('id'),$(this));			
+		});
+	    $("#sempresa").change(function() {
+						getProyectos($(this).children('option').filter(':selected').attr('id'),$(this));			
+		});
+		$("#sproyecto").change(function() {
+						getInfo($(this).children('option').filter(':selected').attr('id'),$(this));			
+		});
+		$('[name="estado"]').change(function() {
+						getGrupo($(this).val());						
+		});
+
+	function getGrupo(valor){
+			/*Datos a mandar para indicar si esta activo o no*/
+			var data={ 
+			's_token':$('#s_token').attr('value'),
+			'activo':valor,
+			};
+			/*Hace la llamada y maneja la respuesta con un popup en caso de que haya habido un error*/
+			$.ajax({
+			     type: "POST",
+			     url: "avancesproyecto/getGrupos",
+			     data: data ,
+			     success: function(msg){
+						console.log(msg);
+						var mensaje = $.parseJSON(msg);
+						if(mensaje['response'] ==  "true"){
+								//Agrega las Empresas al nodo seleccionado
+								var sGrupo = $('#sgrupo');								
+								var sEmpresas = $('#sempresa');
+								var sProyectos = $('#sproyecto');
+								var sCategoria = $('[name = "categoria"]');
+								var sContactos = $('[name = "contacto"]');
+								var sProfesor = $('[name="profesor"]');
+								var sEstados = $('#sestados');
+								var sDocumentos = $('[name = "documento"]');
+								//Elimina nodos
+								$(sGrupo).empty();
+								$(sEmpresas).empty();
+								$(sProyectos).empty();
+								$(sCategoria).empty();
+								$(sProfesor).empty();
+								$(sContactos).empty();
+								$(sEstados).empty();
+								$(sDocumentos).empty();
+								//Agrega los nodos que se buscaron
+								appendGrupo(mensaje['grupo'],sGrupo);
+								appendEmpresas(mensaje['mensaje'],sEmpresas);													
+							    appendProyectos(mensaje['proyectos'],sProyectos);
+								appendCategoria(mensaje['categoria'],sCategoria);
+								appendProfesor(mensaje['usuario'],sProfesor);
+								appendContacto(mensaje['contacto'],sContactos);
+								appendEstado(mensaje['estado'],sEstados);
+								appendDocument(mensaje['documento'],sDocumentos);
+							$('#showThis').attr("style","display:inline-table");
+							$('#showThis2').attr("style","display:inline-table");
+						}else{
+							noty({text: mensaje['mensaje'], type: 'error'});
+						}
+
+			     },
+				error: function(msg){
+						noty({text: "Ha habido un error en el sistema, intentelo nuevamente.", type: 'error'});
+				}
+			});
+	}
+	//Regresa las empresas que pertenecen a un Grupo y las
+	//Inserta en el OptionS List
+	function getEmpresas(idGrupo,elemento){
+			/*Datos de la tabla con Respecto al usuario*/
+			var data={ 
+			's_token':$('#s_token').attr('value'),
+			'idGrupo':idGrupo,
+			'activo':$('[name="estado"]').val(),
+			};
+			/*Hace la llamada y maneja la respuesta con un popup en caso de que haya habido un error*/
+			$.ajax({
+			     type: "POST",
+			     url: "avancesproyecto/getEmpresas",
+			     data: data ,
+			     success: function(msg){
+
+						var mensaje = $.parseJSON(msg);
+						if(mensaje['response'] ==  "true"){
+								//Agrega las Empresas al nodo seleccionado
+								var sEmpresas = $('#sempresa');
+								var sProyectos = $('#sproyecto');
+								var sCategoria = $('[name = "categoria"]');
+								var sContactos = $('[name = "contacto"]');
+								var sProfesor = $('[name="profesor"]');
+								var sEstados = $('#sestados');
+								var sDocumentos = $('[name = "documento"]');
+								
+								//Elimina nodos
+								$(sEmpresas).empty();
+								$(sProyectos).empty();
+								$(sCategoria).empty();
+								$(sProfesor).empty();
+								$(sContactos).empty();
+								$(sEstados).empty();
+								$(sDocumentos).empty();
+								
+								//Agrega los nodos que se buscaron
+								appendEmpresas(mensaje['mensaje'],sEmpresas);			
+								appendProyectos(mensaje['proyectos'],sProyectos);
+								appendCategoria(mensaje['categoria'],sCategoria);
+								appendProfesor(mensaje['usuario'],sProfesor);
+								appendContacto(mensaje['contacto'],sContactos);
+																appendEstado(mensaje['estado'],sEstados);
+																								appendDocument(mensaje['documento'],sDocumentos);
+
+
+						}else{
+							noty({text: mensaje['mensaje'], type: 'error'});
+						}
+
+			     },
+				error: function(msg){
+						noty({text: "Ha habido un error en el sistema, intentelo nuevamente.", type: 'error'});
+				}
+			});
+	}
+	
+	function getProyectos(idEmpresa,elemento){
+			/*Datos de la tabla con Respecto al usuario*/
+			var data={ 
+			's_token':$('#s_token').attr('value'),
+			'idEmpresa':idEmpresa,
+			'activo':$('[name="estado"]').val(),			
+			};
+			/*Hace la llamada y maneja la respuesta con un popup en caso de que haya habido un error*/
+			$.ajax({
+			     type: "POST",
+			     url: "avancesproyecto/getProyectos",
+			     data: data ,
+			     success: function(msg){
+					 	console.log(msg);
+						var mensaje = $.parseJSON(msg);
+						console.log(mensaje);
+						if(mensaje['response'] ==  "true"){
+								//Agrega las Empresas al nodo seleccionado
+								var sProyectos = $('#sproyecto');
+								var sCategoria = $('[name = "categoria"]');
+								var sContactos = $('[name = "contacto"]');							
+								var sProfesor = $('[name="profesor"]');								
+								var sEstados = $('#sestados');
+								var sDocumentos = $('[name = "documento"]');
+								
+								//Elimina nodo
+								$(sProyectos).empty();
+								$(sCategoria).empty();
+								$(sProfesor).empty();
+								$(sContactos).empty();
+								$(sEstados).empty();
+								$(sDocumentos).empty();
+								
+								//Agrega los nodos que se buscaron
+								appendProyectos(mensaje['mensaje'],sProyectos);
+								appendCategoria(mensaje['categoria'],sCategoria);
+								appendProfesor(mensaje['usuario'],sProfesor);
+								appendContacto(mensaje['contacto'],sContactos);
+								appendEstado(mensaje['estado'],sEstados);
+								appendDocument(mensaje['documento'],sDocumentos);
+								
+						
+						}else{
+							noty({text: mensaje['mensaje'], type: 'error'});
+						}
+
+			     },
+				error: function(msg){
+						noty({text: "Ha habido un error en el sistema, intentelo nuevamente.", type: 'error'});
+				}
+			});
+	}
+	function getInfo(idProyecto,elemento){
+			/*Datos de la tabla con Respecto al usuario*/
+			var data={ 
+			's_token':$('#s_token').attr('value'),
+			'idProyecto':idProyecto,
+			'activo':$('[name="estado"]').val(),			
+			};
+			/*Hace la llamada y maneja la respuesta con un popup en caso de que haya habido un error*/
+			$.ajax({
+			     type: "POST",
+			     url: "avancesproyecto/getInfo",
+			     data: data ,
+			     success: function(msg){
+						var mensaje = $.parseJSON(msg);
+						console.log(mensaje);
+						if(mensaje['response'] ==  "true"){
+								//Agrega las Empresas al nodo seleccionado
+								var sCategoria = $('[name = "categoria"]');
+								var sContactos = $('[name = "contacto"]');
+								var sProfesor = $('[name="profesor"]');
+								var sDocumentos = $('[name = "documento"]');
+								
+								//Elimina nodo
+								$(sCategoria).empty();
+								$(sProfesor).empty();
+								$(sContactos).empty();
+																$(sDocumentos).empty();
+
+								//Agrega los nodos que se								 buscaron
+								appendCategoria(mensaje['categoria'],sCategoria);
+								appendProfesor(mensaje['usuario'],sProfesor);
+								appendContacto(mensaje['contacto'],sContactos);
+								appendDocument(mensaje['documento'],sDocumentos);
+								
+						}else{
+							noty({text: mensaje['mensaje'], type: 'error'});
+						}
+
+			     },
+				error: function(msg){
+						noty({text: "Ha habido un error en el sistema, intentelo nuevamente.", type: 'error'});
+				}
+			});
+	}
+
+
+	/*Agrega todos los elementos de un arreglo a un nodo*/
+	function appendEmpresas(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<option>").append("No hay Empresas Registradas").attr('id','0'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				$(elemento).append($("<option>").append(nodo['nombre']).attr('id',nodo['idEmpresa']));
+			});
+		}
+	}	
+	function appendProyectos(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<option>").append("No hay Proyectos registrados").attr('id','0'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				$(elemento).append($("<option>").append(nodo['nombre']).attr('id',nodo['idProyecto']));
+			});
+		}
+	}	
+	function appendGrupo(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<option>").append("No hay Proyectos registrados").attr('id','0'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				$(elemento).append($("<option>").append(nodo['nombre']).attr('id',nodo['idGrupo']));
+			});
+		}
+	}		
+	function appendCategoria(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append('No tiene categoria asignada');
+		}else{
+			$(nodos).each(function(index,nodo){
+			       $(elemento).append(nodo['Categoria']);
+			});
+		}
+	}	
+	function appendProfesor(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<li>").append("No tiene usuarios asignados").attr('id','0'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				$(elemento).append($("<li>").append(nodo['nombre']).attr('id',nodo['idUsuario']));
+			});
+		}
+	}	
+	function appendDocument(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<li>").append("No tiene documentos guardados").attr('id','0'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				if(nodo['esLegal']){
+				$(elemento)
+					.append($("<li>")
+						.append($("<a href>")
+							.append("Legal")
+							.attr("href","avancesProyecto/do_download/1/"+nodo['idProyecto'])
+								)
+								.attr({'id':nodo['idDocumento']})
+							
+						);
+				}
+				else
+				{
+					$(elemento)
+					.append($("<li>")
+						.append($("<a>")
+							.append("Propuesta")
+								.attr({'id':nodo['idDocumento']})
+							).attr("href","avancesProyecto/do_download/0/"+nodo['idProyecto'])
+						);
+				}
+			});
+		}
+	}		
+	function appendContacto(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<li>").append("No hay contactos registrados").attr('id','0'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				$(elemento).append($("<li>").append(nodo['nombre']).attr('id',nodo['idContacto']));
+			});
+		}
+	}	
+	function appendEstado(nodos,elemento){
+		if(nodos.length == 0){
+			$(elemento).append($("<td>").append("No hay status registrados").attr('id','0'));
+			$(elemento).append($("<td>").append("").attr('id','1'));
+			$(elemento).append($("<td>").append("").attr('id','2'));
+		}else{
+			$(nodos).each(function(index,nodo){
+				var tr = $("<tr>");
+				tr.append($("<td>").append(nodo['estado']));
+				tr.append($("<td>").append(nodo['nombre']));
+				tr.append($("<td>").append(nodo['fecha']));
+				$(elemento).append(tr);
+			});
+		}
+	}	
+	});
+
+</script>
