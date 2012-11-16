755691f35eacb5ef284456a201da2b70fe9d0e64
Enrique <quicoide@gmail.com>
2012-11-16 01:31:46 -0600

    Cambios

---
+++ OEV/application/controllers/descargarPropuesta.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class descargarPropuesta
+extends CI_Controller {
+
+    public function index()
+    {
+		//Sesiones
+
+		$this->load->helper('url');
+        $this->load->helper('form');
+		$this->load->model('usuariomodel');
+		$this->load->helper('security');		
+		$this->load->library('session');
+		//Se carga el Modelo de Proyecto
+		$this->load->model('proyecto');
+		//Cargar la sesion y se obtiene el id del usuario
+		$datos_usuario=$this->session->all_userdata();
+		$vista = array('vista'=>$datos_usuario['vista']);
+		$usuario = $datos_usuario['idUsuario'];
+		//Se buscan todos los proyectos relacionados al usuario
+		$proyectos['proyectos'] = $this->proyecto->selectProyectosAceptados($usuario);
+		
+		//Se cargan las Vistas
+		$this->load->view('usuarios/header',$vista);
+		$this->load->view('usuarios/usuario_proyecto/menu_uproyecto');
+        $this->load->view('usuarios/usuario_proyecto/subir_Propuesta',$proyectos);
+		$this->load->view('usuarios/footer');
+		$this->load->view('usuarios/usuario_proyecto/Scripts/subirPropuesta');
+    }
+    
+    function do_download()
+	{
+		$this->load->model('documento');
+		$this->load->helper('url');
+
+		$documento=$this->documento->getDocument(3);
+		$size = $documento[0]->Size;
+		$type = $documento[0]->Extension;
+		$name = $documento[0]->Titulo;
+		$content = $documento[0]->Archivo;
+
+		header("Content-length: ".$size."");
+		header("Content-type: ".$type."");
+		header('Content-Disposition: attachment; filename="'.$name.'"');
+		echo $content;
+	}
+}
+?>
