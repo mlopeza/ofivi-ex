e5166076a1c1d9ff6a3ba118ad75add5c7c11738
Mario Lopez <mario.aleman@hotmail.com>
2012-11-16 01:10:25 -0600

    Se agrego lo de Categorias de Proyectos en Vista de Cliente

---
+++ OEV/application/views/usuarios/usuario_legal/Scripts/subirContratoLegal.php
@@ -0,0 +1,32 @@
+<script type="text/javascript" >
+	/*Logica de Creación de Proyecto*/
+	proyecto_global=-1;
+	$(document).ready(function(){
+		
+		
+		//Se selecciona un Proyecto
+        $(".colorea-proyecto").live("click",function(){
+                $.each($(".colorea-proyecto"),function(index,elemento){
+                    $(elemento).attr("style","");
+                });
+
+                $(this).attr("style","background-color:whiteSmoke;");
+                proyecto_global=$(this).attr('idproyecto');
+                
+	            console.log("idProyecto = "+proyecto_global);
+                $("#idProyectoContrato").val(proyecto_global);
+        });
+	
+		//Guarda los cambios al reporte
+        $("#uploadFile").click(function(){
+            if(proyecto_global==-1){
+				noty({text: "No se ha escogido ningún proyecto.", type: 'error'});
+                return;
+            }
+            
+            noty({text: "Se subio el contrato.", type: 'success'});
+            setTimeout(function() { location.reload(); }, 3000);
+            $("#subir-contrato").submit();
+        });
+	});
+</script>
